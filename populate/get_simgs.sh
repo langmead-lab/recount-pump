4b4e2bb173de248b1eb14072a166730025ffaebe
BenLangmead <langmea@cs.jhu.edu>
2017-11-25 09:23:53 -0500

    flip sense of test

--- populate/get_simgs.sh
+++ populate/get_simgs.sh
@@ -2,7 +2,7 @@
 
 IMAGES="hisat2 star"
 
-[ -n "${SINGULARITY_CACHEDIR}" ] && echo "Set SINGULARITY_CACHEDIR to destination dir" && exit 1
+[ -z "${SINGULARITY_CACHEDIR}" ] && echo "Set SINGULARITY_CACHEDIR to destination dir" && exit 1
 
 for nm in $IMAGES ; do
     singularity pull --name "${SINGULARITY_CACHEDIR}/${nm}" shub://langmead-lab/recount-pump:${nm}
