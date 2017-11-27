f805522acefa97de366a324487ae40dc0114ffb0
BenLangmead <langmea@cs.jhu.edu>
2017-11-26 21:14:19 -0500

    add script for uploading all

---
+++ populate/synapse_upload_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for i in `find -name gtf | sed 's/^\.\///' | sed 's/\/.*//'` ; do
+    cat >.$i.upload.sh <<EOF
+#SBATCH
+#SBATCH --job-name=upload_${i}
+#SBATCH --partition=parallel
+#SBATCH --output=.$i.upload.sh.o
+#SBATCH --error=.$i.upload.sh.e
+#SBATCH --nodes=1
+#SBATCH --mem=1G
+#SBATCH --partition=shared
+#SBATCH --time=8:00:00
+sh synapse_upload.sh $i
+EOF
+    echo "sbatch .$i.upload.sh"
+done
