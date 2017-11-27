685d5b3fd8f2d0c282c89ce0a0418b981dc9f412
BenLangmead <langmea@cs.jhu.edu>
2017-11-26 20:56:48 -0500

    handle subdirs the way I intended

--- populate/synapse_upload.sh
+++ populate/synapse_upload.sh
@@ -12,9 +12,19 @@ ref_id=`cat ${scr_dir}/synapse_references_id.txt`
 
 [ -z "${ref_id}" ] && echo "Could not get ref_id" && exit 1
 
-tar -cvf - ${dr} | gzip -c > ${dr}.tar.gz
+# Create new subdirectory for this species
+species_id=`synapse create -parentid ${ref_id} -name ${dr} | awk '{print $3}'`
+[ -z "${species_id}" ] && echo "Could not get species_id" && exit 1
+
+# For populating the description field
 host=`hostname -f`
 dt=`date`
-md5=`md5sum ${dr}.tar.gz | awk '{print $1}'`
-synapse add -parentid ${ref_id} --description "Uploaded from ${host}, ${dt}, md5=${md5}" ${dr}.tar.gz
-rm -f ${dr}.tar.gz
+
+cd ${dr}
+for d in * ; do
+    tar -cvf - ${d} | gzip -c > ${d}.tar.gz
+    md5=`md5sum ${d}.tar.gz | awk '{print $1}'`
+    synapse add -parentid ${species_id} --description "Uploaded from ${host}, ${dt}, md5=${md5}" ${d}.tar.gz
+    rm -f ${d}.tar.gz
+done
+cd ..
