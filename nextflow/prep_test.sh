81ae3792f0337e220fccc6bb4cfa83baceff52e5
Ben Langmead <langmea@cs.jhu.edu>
2018-01-08 14:57:59 -0500

    explode tarballs

--- nextflow/prep_test.sh
+++ nextflow/prep_test.sh
@@ -5,6 +5,9 @@ set -ex
 bash ../populate/synapse_download.sh ce10
 mkdir ref
 mv ce10 ref
+cd ref/ce10
+for i in *.tar.gz ; do tar zxvf $i ; done
+cd ../..
 
 mkdir -p input
 cp accessions.txt input
