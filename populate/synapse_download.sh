55d8e7b032153d164306bbb61f00baecce6b11e4
Ben Langmead <langmea@cs.jhu.edu>
2018-01-08 14:28:32 -0500

    get references id from same dir

--- populate/synapse_download.sh
+++ populate/synapse_download.sh
@@ -2,11 +2,13 @@
 
 # Download reference files from synapse and expand 
 
+d=`dirname $0`
+
 set -e
 
 species=$1
 [ -z "${species}" ] && echo "specify species shortname (e.g. dm6) as argument"
-refid=`cat synapse_references_id.txt`
+refid=`cat $d/synapse_references_id.txt`
 synid=`synapse list ${refid} | grep ${species} | awk '{print $1}'`
 
 test -n $synid
