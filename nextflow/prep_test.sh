9cb38195c72c6a703485e791eb3ffb79d6a89126
Ben Langmead <langmea@cs.jhu.edu>
2018-01-08 17:45:57 -0500

    use S3

--- nextflow/prep_test.sh
+++ nextflow/prep_test.sh
@@ -2,9 +2,14 @@
 
 set -ex
 
-bash ../populate/synapse_download.sh ce10
-mkdir ref
-mv ce10 ref
+species=ce10
+url=https://s3.amazonaws.com/recount-pump/ref
+
+mkdir -p ref/${species}
+for i in hisat2_idx.tar.gz fasta.tar.gz gtf.tar.gz ; do
+    wget -O ref/${species}/${i} ${url}/${species}/${i}
+done
+
 cd ref/ce10
 for i in *.tar.gz ; do tar zxvf $i ; done
 cd ../..
