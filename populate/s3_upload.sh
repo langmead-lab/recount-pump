308717d3ea3456215816e06f31caab5ef0295392
Ben Langmead <langmea@cs.jhu.edu>
2018-01-09 10:40:12 -0500

    add TODO to upload script

--- populate/s3_upload.sh
+++ populate/s3_upload.sh
@@ -3,4 +3,7 @@
 species=$1
 [ -z "${species}" ] && echo 'specify species as first arg'
 
-for i in ${species}/* ; do aws s3 cp $i s3://recount-pump/ref/${species}/`basename $i` ; done
+for i in ${species}/* ; do
+    aws s3 cp $i s3://recount-pump/ref/${species}/`basename $i`
+    # TODO: make public
+done
