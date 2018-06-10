6b9cb517145d0ccdc73390dea29ea9806093d658
Ben Langmead <langmea@cs.jhu.edu>
2018-06-10 15:27:39 -0400

    fix imports

--- src/queueing/service.py
+++ src/queueing/service.py
@@ -8,14 +8,14 @@ For more on how to install a local RabbitMQ service using Docker, see
 /software/rabbitmq in this repo.
 """
 
-import rmq
-import sqs
+import queueing.rmq
+import queueing.sqs
 
 
 def get_queueing_service(type='rmq'):
     if type == 'rmq':
-        return rmq.RmqService()
+        return queueing.rmq.RmqService()
     elif type == 'sqs':
-        return sqs.SqsService()
+        return queueing.sqs.SqsService()
     else:
         raise RuntimeError('Bad queueing service type "%s"' % type)
