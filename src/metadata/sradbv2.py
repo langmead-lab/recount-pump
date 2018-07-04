ee2964189140c5486d1f915aedce4c0e4874fbc0
Ben Langmead <langmea@cs.jhu.edu>
2018-07-03 20:41:35 -0400

    switch to docopt; begin search function

--- src/metadata/sradbv2.py
+++ src/metadata/sradbv2.py
@@ -1,20 +1,48 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 # Authors: Chris Wilks (original) and Ben Langmead (modifications)
 #    Date: 7/3/2018
+# License: MIT
 
+"""sradbv2
+
+Usage:
+  sradbv2 search <lucene-search> [options]
+  sradbv2 query [<SRP>,<SRR>]...  [options]
+  sradbv2 query-file <file> [options]
+
+Options:
+  --delay                  Seconds to sleep between requests [default: 1].
+  --log-ini <ini>          .ini file for log aggregator [default: ~/.recount/log.ini].
+  --log-section <section>  .ini file section for log aggregator [default: log].
+  --log-level <level>      Set level for log aggregation; could be CRITICAL,
+                           ERROR, WARNING, INFO, DEBUG [default: INFO].
+  -a, --aggregate          Enable log aggregation.
+  --noheader               Suppress header in output.
+  --nostdout               Output to files named by study accession.
+  -h, --help               Show this screen.
+  --version                Show version.
+"""
+
+from __future__ import print_function
 import sys
 import json
 import cgi
 try:
     from urllib.request import urlopen
+    from urllib.parse import quote
 except ImportError:
+    from urllib import quote
     from urllib2 import urlopen
-import argparse
 import time
 import os
+from docopt import docopt
+import log
 
-SRAdbV2_URL = 'https://api-omicidx.cancerdatasci.org/sra/1.0/full'
+
+sradbv2_api_url = 'https://api-omicidx.cancerdatasci.org/sra/1.0'
+sradbv2_full_url = sradbv2_api_url + '/full'
+sradbv2_search_url = sradbv2_api_url + '/search/full'
 KEY_VALUE_DELIM = '::'
 SUBFIELD_DELIM = '|'
 
@@ -23,8 +51,8 @@ nested_fields = {'attributes': ['tag', 'value'],
                  'xrefs': ['db', 'id']}
 
 
-def download_and_extract_fields(args, run_acc, study_acc, header_fields):
-    url = "%s/%s" % (SRAdbV2_URL, run_acc)
+def download_and_extract_fields(run_acc, study_acc, header_fields):
+    url = "%s/%s" % (sradbv2_full_url, run_acc)
     response = urlopen(url)
     j, ct = cgi.parse_header(response.headers.get('Content-type', ''))
     encodec = ct.get('charset', 'utf-8')
@@ -35,8 +63,7 @@ def download_and_extract_fields(args, run_acc, study_acc, header_fields):
     header_fields.update(jfields['_source'].keys())
 
 
-def process_run(args, run_acc, study_acc, header_fields, outfile):
-    jfields = None
+def process_run(run_acc, study_acc, header_fields, outfile):
     infile = "%s.%s.json.temp" % (run_acc, study_acc)
     with open(infile, "rb") as fin:
         jfields = json.load(fin)
@@ -77,68 +104,78 @@ def process_run(args, run_acc, study_acc, header_fields, outfile):
     outfile.write('\n')
 
 
-def process_study(args, study_acc, runs_per_study, header_fields):
+def process_study(study_acc, runs_per_study, header_fields, nostdout, noheader):
     header_fields = sorted(list(header_fields))
     outfile = sys.stdout
-    if args.nostdout:
+    if nostdout:
         outfile = open('%s.metadata.tsv' % study_acc, 'wb')
-    if not args.noheader:
+    if not noheader:
         outfile.write('\t'.join(header_fields) + '\n')
-    [process_run(args, run_acc, study_acc, header_fields, outfile) for run_acc in runs_per_study]
-    if args.nostdout:
+    [process_run(run_acc, study_acc, header_fields, outfile) for run_acc in runs_per_study]
+    if nostdout:
         outfile.close()
 
-def query_iterator(args):
-    if args.query_file is not None:
-        with open(args.query_file, "rb") as fin:
-            for line in fin:
-                study_acc, run_acc = line.rstrip().split(b'\t')
-                yield study_acc, run_acc
-    elif args.queries is not None:
-        for pair in args.queries.split(';'):
-            if 2 != len(pair.split(',')):
-                raise ValueError('Bad argument to --queries')
-            study_acc, run_acc = pair.split(',')
+
+def query_string_iterator(sts):
+    for st in sts:
+        if not st.count(',') == 1:
+            raise ValueError('Bad query argument: "%s"' % st)
+        study_acc, run_acc = st.split(',')
+        yield study_acc, run_acc
+
+
+def query_file_iterator(fn):
+    with open(fn, "rb") as fin:
+        for line in fin:
+            study_acc, run_acc = line.rstrip().split(b'\t')
             yield study_acc, run_acc
-    else:
-        raise RuntimeError('Must specify either --queries or --query-file')
 
 
-def main(args):
+def query(delay, nostdout, noheader, query_string=None, query_file=None):
     prev_study = None
     runs_per_study = set()
     header_fields = set()
-    for study_acc, run_acc in query_iterator(args):
+    header_fields_vec = []
+    it = query_string_iterator(query_string) if query_string else query_file_iterator(query_file)
+    for study_acc, run_acc in it:
         if study_acc != prev_study:
             if prev_study is not None:
-                process_study(args, prev_study, runs_per_study, header_fields)
+                process_study(prev_study, runs_per_study, header_fields, nostdout, noheader)
             runs_per_study = set()
             header_fields = set()
         prev_study = study_acc
         runs_per_study.add(run_acc)
-        download_and_extract_fields(args, run_acc, study_acc, header_fields)
-        if args.delay > 0:
-            time.sleep(args.delay)
+        download_and_extract_fields(run_acc, study_acc, header_fields)
+        header_fields_vec.append(len(header_fields))
+        if delay > 0:
+            time.sleep(delay)
     if prev_study is not None:
-        process_study(args, prev_study, runs_per_study, header_fields)
+        process_study(prev_study, runs_per_study, header_fields, nostdout, noheader)
+    print(str(header_fields_vec), file=sys.stderr)
 
 
-def create_parser(disable_header=False):
-    parser = argparse.ArgumentParser(
-        description='Given a list of SRA run accessions (e.g. SRR2126175) pull down the full metadata from the SRAdbV2 API')
-    parser.add_argument('--query-file', metavar='/path/to/file_with_run_accessions', type=str, default=None,
-                        help='path to file with list of SRA run accessions')
-    parser.add_argument('--queries', metavar='<study1>,<run1>;<study2>,<run2>;...', type=str, default=None,
-                        help='list of study/run accession pairs')
-    parser.add_argument('--delay', type=int, default=1, help='time to sleep between requests, defaults to 1 sec (1)')
-    parser.add_argument('--noheader', action='store_const', const=True, default=False,
-                        help='turn off printing header in output')
-    parser.add_argument('--nostdout', action='store_const', const=True, default=False,
-                        help='output to files named by study accession')
-    return parser
+def process_search(search):
+    url = sradbv2_search_url + '?' + quote(search)
+    response = urlopen(url)
+    j, ct = cgi.parse_header(response.headers.get('Content-type', ''))
+    encodec = ct.get('charset', 'utf-8')
+    log.info(__name__, 'Charset: ' + encodec, 'sradbv2.py')
+    payload = response.read().decode(encodec)
 
 
 if __name__ == '__main__':
-    parser = create_parser()
-    args = parser.parse_args()
-    main(args)
+    args = docopt(__doc__)
+    agg_ini = os.path.expanduser(args['--log-ini']) if args['--aggregate'] else None
+    log.init_logger(__name__, aggregation_ini=agg_ini,
+                     aggregation_section=args['--log-section'],
+                     agg_level=args['--log-level'])
+    try:
+        if args['search']:
+            process_search(args['<lucene-search>'])
+        elif args['query']:
+            query(args['--delay'], args['--nostdout'], args['--noheader'], query_string=args['<SRP>,<SRR>'])
+        elif args['query-file']:
+            query(args['--delay'], args['--nostdout'], args['--noheader'], query_file=args['<file>'])
+    except Exception:
+        log.error(__name__, 'Uncaught exception:', 'sradbv2.py')
+        raise
