186f76db5ee7132abede12f5afb3818fb8b484ae
Ben Langmead <langmea@cs.jhu.edu>
2018-07-16 15:00:43 -0400

    fix issue of extra ref subdir

--- workflow/prep_test.sh
+++ workflow/prep_test.sh
@@ -2,13 +2,15 @@
 
 set -ex
 
-species=ce10
 url=https://s3.amazonaws.com/recount-pump/ref
 
-mkdir -p ref/${species}
-for i in hisat2_idx.tar.gz fasta.tar.gz gtf.tar.gz ; do
-    wget -q -O ref/${species}/${i} ${url}/${species}/${i}
+for species in ce10 ; do    
+    mkdir -p ${species}
+    for i in hisat2_idx.tar.gz fasta.tar.gz gtf.tar.gz ; do
+        wget -q -O ${species}/${i} ${url}/${species}/${i}
+    done
+    
+    cd ${species}
+    for i in *.tar.gz ; do tar zxf $i ; done
+    cd ..
 done
-
-cd ref/ce10
-for i in *.tar.gz ; do tar zxvf $i ; done
