322adde482c6c5f5dfb7044d065feed16313c802
Ben Langmead <langmea@cs.jhu.edu>
2018-07-16 15:36:37 -0400

    add -c

--- workflow/rna-seq/singrun.sh
+++ workflow/rna-seq/singrun.sh
@@ -72,10 +72,11 @@ export OUTPUT="/recount-output" && \
 export RECOUNT_REF="/recount-ref" && \
 export RECOUNT_TEMP="/recount-temp" && \
 singularity exec \
-    --bind ${input_dir}:${INPUT} \
-    --bind ${output_dir}:${OUTPUT} \
-    --bind ${REF2}:${RECOUNT_REF} \
-    --bind ${TEMP2}:${RECOUNT_TEMP} \
+    -c \
+    -B ${input_dir}:${INPUT} \
+    -B ${output_dir}:${OUTPUT} \
+    -B ${REF2}:${RECOUNT_REF} \
+    -B ${TEMP2}:${RECOUNT_TEMP} \
     ${image} \
     /bin/bash -c \
     "source activate rnaseq && bash /tmp/rna_seq.bash"
