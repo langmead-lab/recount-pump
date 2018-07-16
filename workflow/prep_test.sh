1a81d813c8d36fab7d82c25d7efa5bf54c08d273
Ben Langmead <langmea@cs.jhu.edu>
2018-07-16 14:44:53 -0400

    hush wget

--- workflow/prep_test.sh
+++ workflow/prep_test.sh
@@ -7,7 +7,7 @@ url=https://s3.amazonaws.com/recount-pump/ref
 
 mkdir -p ref/${species}
 for i in hisat2_idx.tar.gz fasta.tar.gz gtf.tar.gz ; do
-    wget -O ref/${species}/${i} ${url}/${species}/${i}
+    wget -q -O ref/${species}/${i} ${url}/${species}/${i}
 done
 
 cd ref/ce10
