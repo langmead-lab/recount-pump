d9e7013820b988ac0e99d3e048f33b0a7793d6f3
Ben Langmead <langmea@cs.jhu.edu>
2018-07-18 21:54:57 -0400

    run on localhost port 25432

--- docker/postgres/run.sh
+++ docker/postgres/run.sh
@@ -4,4 +4,4 @@ docker run --rm --name postgres-recount \
     -e POSTGRES_USER=recount \
     -e POSTGRES_PASSWORD=recount-postgres \
     -e POSTGRES_DB=recount-test \
-    -p 5432:5432 -d postgres:10.4
+    -p 25432:5432 -d postgres:10.4
