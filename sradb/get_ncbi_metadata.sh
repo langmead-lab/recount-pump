814234618d1a4af37c8c6a8c6249226739a5a128
Ben Langmead <langmea@cs.jhu.edu>
2018-08-13 21:34:45 -0400

    remove old sradb scripts

--- sradb/get_ncbi_metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-date=20171103
-
-wget ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata/NCBI_SRA_Metadata_Full_${date}.tar.gz
--- sradb/load_db_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/local/bin/R -e "rmarkdown::render('load_db.Rmd',output_file='load_db_output.html')"
