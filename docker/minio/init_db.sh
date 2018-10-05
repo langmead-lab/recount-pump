c494925ee0fdf8950cf516baf36fa4d46140c07e
Ben Langmead <langmea@cs.jhu.edu>
2018-10-04 22:43:51 -0400

    stabilize date range

--- docker/minio/init_db.sh
+++ docker/minio/init_db.sh
@@ -19,7 +19,7 @@ setup_refs() {
 setup_metadata() {
     mkdir -p $STAGING/meta/ce10_test
     cd /tmp/src
-    python -m metadata.sradbv2 search 'sample_taxon_id:6239 AND experiment_library_strategy:"rna seq" AND experiment_library_source:transcriptomic AND experiment_platform:illumina' --gzip --output $STAGING/meta/ce10_test/ce10_test.json
+    python -m metadata.sradbv2 search 'sample_taxon_id:6239 AND experiment_library_strategy:"rna seq" AND experiment_library_source:transcriptomic AND experiment_platform:illumina AND run_FileDate:["2017-01-01" TO "2017-05-01"]' --gzip --output $STAGING/meta/ce10_test/ce10_test.json
 }
 
 setup_metadata
