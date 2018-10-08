e98e8ff335a69b68f99f61510da6ec9192774942
Ben Langmead <langmea@cs.jhu.edu>
2018-10-07 21:47:03 -0400

    add delete script

---
+++ logs/delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+profile=$(grep aws_profile ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
+log_group=$(grep log_group ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
+log_stream=$1
+shift
+
+if [[ -z "${log_stream}" ]] ; then
+    log_stream=$(grep stream_name ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
+fi
+
+aws logs --profile ${profile} \
+         --log-group-name=${log_group} \
+         --log-stream-name=${log_stream} \
+         delete-log-stream
