08f4058e85efd2426f091db09e997856b0576f0d
Ben Langmead <langmea@cs.jhu.edu>
2018-10-14 11:50:34 -0400

    fix bucket

--- populate/s3_upload.sh
+++ populate/s3_upload.sh
@@ -4,6 +4,6 @@ species=$1
 [ -z "${species}" ] && echo 'specify species as first arg'
 
 for i in ${species}/* ; do
-    aws s3 cp $i s3://recount-pump/ref/${species}/`basename $i`
+    aws s3 cp $i s3://recount-ref/${species}/`basename $i`
     # TODO: make public
 done
