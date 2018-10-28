889a58c6a3c7b8c8c40ae0a23f08f4cb8bd3ecef
Ben Langmead <langmea@cs.jhu.edu>
2018-10-27 19:39:16 -0700

    add TAIR upload

---
+++ projects/arabidopsis/upload_tair_parts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+species=TAIR10_Araport11
+
+for i in hisat2_idx.tar.gz gtf.tar.gz fasta.tar.gz ucsc_tracks.tar.gz ; do
+    aws --profile jhu-langmead s3 cp --acl public-read ${i} s3://recount-ref/${species}/${i}
+done
