55e530ca75de953a6e146ada89a4759cf5b34683
Ben Langmead <langmea@cs.jhu.edu>
2018-10-29 23:01:30 -0400

    simplify allen metadata retrieval

--- projects/allen/get_meta.sh
+++ projects/allen/get_meta.sh
@@ -9,7 +9,7 @@ srp=SRP061902
 
 get_metadata() {
     pushd $d/../../src
-    python -m metadata.sradbv2 search "study_accession:${srp} AND experiment_library_strategy:\"rna seq\" AND experiment_library_source:transcriptomic AND experiment_platform:illumina" --gzip --output ${study}.json
+    python -m metadata.sradbv2 search "study_accession:${srp}" --gzip --output ${study}.json
     popd
     mv $d/../../src/${study}.json.gz $d/
 }
