08796e7406abd4867750319c7f4f8883d2fbc1bc
Ben Langmead <langmea@cs.jhu.edu>
2018-10-31 11:45:53 -0400

    record some useful URLs for tair/araport

---
+++ populate/arabidopsis_tair10_ara11.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Arabidopsis_thaliana/Ensembl/TAIR10/Arabidopsis_thaliana_Ensembl_TAIR10.tar.gz
+wget https://www.araport.org/download_file/Araport11_Release_201606/annotation/Araport11_GFF3_genes_transposons.201606.gtf.gz
