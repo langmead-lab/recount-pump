b115d47e1193b32589ba6e4a3488155a3c145ea2
Ben Langmead <langmea@cs.jhu.edu>
2018-11-11 09:24:55 -0500

    use docker.sock to connect images inside image

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ d=$(dirname $0)
 
 IMAGE=$(cat ${d}/image.txt)
 
-docker run -it $* --name recount ${IMAGE} /bin/bash
+docker run -it \
+    -v `pwd`:/work \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    $* --name recount --rm ${IMAGE} /bin/bash
