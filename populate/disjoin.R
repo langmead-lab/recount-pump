b17b4b2a479375e4f2977da07b7b08dcaa972729
Ben Langmead <langmea@cs.jhu.edu>
2018-11-11 09:33:39 -0500

    updated disjoin script

--- populate/disjoin.R
+++ populate/disjoin.R
@@ -24,10 +24,15 @@ library(rtracklayer)
 
 opts <- docopt(doc, commandArgs(T))
 
-gtf <- makeTxDbFromGFF(opts[['<url>']], format='gtf',
-    dataSource=opts[['--source']], organism=opts[['--organism']],
-    taxonomyId=as.integer(opts[['--taxid']]))
+url <- opts[['<url>']]
+source_name <- opts[['--source']]
+organism_name <- opts[['--organism']]
+tax_id <- as.integer(opts[['--taxid']])
+exclude_olaps <- as.logical(opts['--exclude-olaps'])
+outfn <- opts[['<outfile>']]
 
-export(exonicParts(gtf,
-    linked.to.single.gene.only=as.logical(opts['--exclude-olaps'])),
-    opts[['<outfile>']], 'bed')
+gtf <- makeTxDbFromGFF(url, format='gtf',  dataSource=source_name,
+                       organism=organism_name, taxonomyId=tax_id)
+
+export(exonicParts(gtf, linked.to.single.gene.only=exclude_olaps),
+       outfn, 'bed')
