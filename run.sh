2e475932ee27aa16b64e2953dbcebcb9600f9321
Ben Langmead <langmea@cs.jhu.edu>
2018-11-14 14:12:40 -0500

    add commands that enable singularity in docker

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ d=$(dirname $0)
 IMAGE=$(cat ${d}/image.txt)
 
 docker run -it \
+    --privileged \
+    --cap-add=SYS_ADMIN \
     -v `pwd`:/work \
     -v /var/run/docker.sock:/var/run/docker.sock \
     $* --name recount --rm ${IMAGE} /bin/bash
