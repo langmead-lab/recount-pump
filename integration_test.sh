20894049939574b8da94b1732f8deac9329d3c1e
Ben Langmead <langmea@cs.jhu.edu>
2018-11-16 21:53:52 -0500

    toward a vagrant-based integration test framework that avoids containers in containers

--- integration_test.sh
+++ integration_test.sh
@@ -2,7 +2,26 @@
 
 set -ex
 
-rm -rf testing/output/*
-rm -rf testing/temp/*
-docker-compose rm -f
-docker-compose up --abort-on-container-exit
+docker-compose up -d
+sleep 15
+
+export AWS_ACCESS_KEY_ID=minio
+export AWS_SECRET_ACCESS_KEY=minio123
+export RECOUNT_TEST_DB="postgres://recount:recount-postgres@localhost:25432/recount-test"
+export RECOUNT_TEST_Q="http://localhost:29324"
+export RECOUNT_TEST_S3="http://localhost:29000"
+export RECOUNT_IMAGES="$(pwd)/testing/images"
+export SINGULARITY_CACHEDIR="$(pwd)/testing/images"
+
+./unit_test.sh
+
+export RECOUNT_CREDS="~/.creds_integration_test"
+
+rm -rf ~/.creds_integration_test
+cp -r ~/git/recount-pump/creds ~/.creds_integration_test
+for i in $(ls ~/.creds_integration_test/*.override) ; do
+    echo "*** Renaming ${i} ***"
+    mv ${i} $(echo ${i} | sed 's/\.override$//')
+done
+
+./e2e_test.sh
