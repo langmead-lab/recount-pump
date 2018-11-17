03e3a83f685eedf5211596a5a1591558381cabb0
Ben Langmead <langmea@cs.jhu.edu>
2018-11-17 18:06:20 -0500

    more automated counter/metrics handling

--- aws/logs/put-metrics.sh
+++ aws/logs/put-metrics.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+d=$(dirname $0)
+
 profile=$(grep aws_profile ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
 log_group=$(grep log_group ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
 log_stream=$1
@@ -8,11 +10,20 @@ if [[ -z "${log_stream}" ]] ; then
     log_stream=$(grep stream_name ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
 fi
 
-echo "Adding filter"
+echo "Adding filters"
 
-aws logs put-metric-filter \
-         --profile ${profile} \
-         --log-group-name=${log_group} \
-         --filter-name JsonTest \
-         --filter-pattern "[ month, day, time, host, module, lab = \"COUNT\", name, amt ]" \
-         --metric-transformations metricName=JsonSumTest,metricNamespace=recount,metricValue=\$amt,defaultValue=0
+metrics=$(grep 'log.*COUNT_' $d/../../src/*.py | sed 's/.*COUNT/COUNT/' | sed 's/ .*//')
+
+echo "  found counters:"
+echo "${metrics}"
+
+for metric in ${metrics} ; do
+    shortname=$(echo ${metric} | sed 's/^COUNT_//')
+    echo "Name: ${metric}, short name: ${shortname}"
+    aws logs put-metric-filter \
+             --profile ${profile} \
+             --log-group-name=${log_group} \
+             --filter-name "${shortname}" \
+             --filter-pattern "[ month, day, time, host, module, lab = \"${metric}\", name, amt ]" \
+             --metric-transformations metricName=JsonSumTest,metricNamespace=recount,metricValue=\$amt,defaultValue=0
+done
