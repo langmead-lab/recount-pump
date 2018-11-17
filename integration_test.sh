c9af5c42935b8153e4385fd0763892b9c47160b0
Ben Langmead <langmea@cs.jhu.edu>
2018-11-17 14:32:31 -0500

    fix settings for analysis dir and creds

--- integration_test.sh
+++ integration_test.sh
@@ -10,12 +10,14 @@ export AWS_SECRET_ACCESS_KEY=minio123
 export RECOUNT_TEST_DB="postgres://recount:recount-postgres@localhost:25432/recount-test"
 export RECOUNT_TEST_Q="http://localhost:29324"
 export RECOUNT_TEST_S3="http://localhost:29000"
-export RECOUNT_IMAGES="$(pwd)/testing/images"
-export SINGULARITY_CACHEDIR="$(pwd)/testing/images"
+
+# Needs to match cluster.ini
+export RECOUNT_IMAGES="${HOME}/recount-images"
+export SINGULARITY_CACHEDIR="${RECOUNT_IMAGES}"
 
 ./unit_test.sh
 
-export RECOUNT_CREDS="~/.creds_integration_test"
+export RECOUNT_CREDS="$HOME/.creds_integration_test/.recount"
 
 rm -rf ~/.creds_integration_test
 cp -r ~/git/recount-pump/creds ~/.creds_integration_test
