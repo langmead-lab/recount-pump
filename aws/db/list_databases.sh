8a146ed808bb20a14eb6bd44cc263040b083fc2d
Ben Langmead <langmea@cs.jhu.edu>
2018-11-18 13:52:21 -0500

    allow list_databases to be called from other dir

--- aws/db/list_databases.sh
+++ aws/db/list_databases.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./connect.sh postgres 'SELECT datname FROM pg_database WHERE datistemplate = false;'
+d=$(dirname $0)
+
+$d/connect.sh postgres 'SELECT datname FROM pg_database WHERE datistemplate = false;'
