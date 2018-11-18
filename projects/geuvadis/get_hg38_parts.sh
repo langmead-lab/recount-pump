5420025bb81dcd1a0c78a428ea6150d58626c675
Ben Langmead <langmea@cs.jhu.edu>
2018-11-18 13:40:47 -0500

    add star_idx

--- projects/geuvadis/get_hg38_parts.sh
+++ projects/geuvadis/get_hg38_parts.sh
@@ -4,6 +4,7 @@ syns=""
 syns="${syns} syn11511600"  # hisat2 index
 syns="${syns} syn11511596"  # fasta
 syns="${syns} syn11511597"  # gtf
+syns="${syns} syn11511606"  # STAR index
 
 for syn in ${syns} ; do
     synapse get ${syn}
--- projects/geuvadis/upload_hg38_parts.sh
+++ projects/geuvadis/upload_hg38_parts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in hisat2_idx.tar.gz gtf.tar.gz fasta.tar.gz ; do
+for i in star_idx.tar.gz gtf.tar.gz fasta.tar.gz ; do
     aws --profile jhu-langmead s3 cp --acl public-read ${i} s3://recount-ref/hg38/${i}
 done
