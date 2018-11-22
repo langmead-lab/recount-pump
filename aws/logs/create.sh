1df3910c43a23fb6ac34b1065a097df430713a2b
Ben Langmead <langmea@cs.jhu.edu>
2018-11-21 21:03:32 -0500

    add region

--- aws/logs/create.sh
+++ aws/logs/create.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-profile=$(grep aws_profile ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
-log_group=$(grep log_group ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
+INI_FILE="$HOME/.recount/log.ini"
+[[ ! -f $INI_FILE ]] && echo "Could not find INI file: ${INI_FILE}" && exit 1
+
+profile=$(grep '^aws_profile' ${INI_FILE} | cut -d"=" -f2 | tr -d '[:space:]')
+log_group=$(grep '^log_group' ${INI_FILE} | cut -d"=" -f2 | tr -d '[:space:]')
+region=$(grep '^region' ${INI_FILE} | cut -d"=" -f2 | tr -d '[:space:]')
 log_stream=$1
 shift
 
@@ -9,7 +13,10 @@ if [[ -z "${log_stream}" ]] ; then
     log_stream=$(grep stream_name ~/.recount/log.ini | cut -d"=" -f2 | tr -d '[:space:]')
 fi
 
+echo "Creating stream"
+
 aws logs --profile ${profile} \
+         --region=${region} \
          --log-group-name=${log_group} \
          --log-stream-name=${log_stream} \
          create-log-stream
