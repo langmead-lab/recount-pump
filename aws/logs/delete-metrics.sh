7a6ad7dd4e80597d1f2e721d2af0e1671ac16a0d
Ben Langmead <langmea@cs.jhu.edu>
2018-11-23 17:31:34 -0500

    factor out parsing; parse wall-clock metrics

--- aws/logs/delete-metrics.sh
+++ aws/logs/delete-metrics.sh
@@ -16,16 +16,9 @@ fi
 
 echo "Adding filters"
 
-metrics=$(grep 'log.*COUNT_' $d/../../src/*.py | sed 's/.*COUNT/COUNT/' | sed 's/ .*//')
-metrics="${metrics} $(grep 'echo \"COUNT_' $d/../../workflow/*/Snakefile | sed 's/.*COUNT/COUNT/' | sed 's/ .*//')"
-metrics=$(echo ${metrics} | sort -u)
-
-echo "  found counters:"
-echo "${metrics}"
-
 set -e
 
-for metric in ${metrics} ; do
+for metric in $($d/parse_counters.sh) ; do
     shortname=$(echo ${metric} | sed 's/^COUNT_//')
     echo "Name: ${metric}, short name: ${shortname}"
     aws logs delete-metric-filter \
