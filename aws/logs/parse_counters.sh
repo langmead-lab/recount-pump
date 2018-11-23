e46ddfeaf6ae717617a6190b00cc2b08855fe384
Ben Langmead <langmea@cs.jhu.edu>
2018-11-23 17:31:49 -0500

    factored out counter parsing

---
+++ aws/logs/parse_counters.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+d=$(dirname $0)
+
+metrics=$(grep 'log.*COUNT_' $d/../../src/*.py | sed 's/.*COUNT/COUNT/' | sed 's/ .*//' | grep -v '%')
+metrics="${metrics} $(grep 'echo \"COUNT_' $d/../../workflow/*/Snakefile | sed 's/.*COUNT/COUNT/' | sed 's/ .*//' | grep -v '%')"
+metrics="${metrics} $(python $d/../../src/stats.py snakefile $d/../../workflow/*/Snakefile)"
+metrics=$(echo ${metrics} | sort -u)
+
+echo ${metrics}
