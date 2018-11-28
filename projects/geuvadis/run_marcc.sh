0d841352badb58a0b0c0758a9ba843a6b09b4f90
Ben Langmead <ben.langmead@gmail.com>
2018-11-28 11:00:24 -0500

    raise time limit for marcc

--- projects/geuvadis/run_marcc.sh
+++ projects/geuvadis/run_marcc.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --mem=40G
-#SBATCH --time=12:00:00
+#SBATCH --time=44:00:00
 #SBATCH --ntasks-per-node=8
 
 d=/scratch/users/blangme2@jhu.edu/git/recount-pump
