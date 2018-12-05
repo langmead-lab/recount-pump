d8f3461007fe9f605ef6e049f09f649e31ca223b
Ben Langmead <ben.langmead@gmail.com>
2018-12-05 12:21:07 -0600

    add knl job

---
+++ projects/srav1/run_batch_knl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=srav1
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH -A TG-CIE170020
+
+d=/work/04265/benbo81/stampede2/git/recount-pump
+
+python ${d}/src/cluster.py run \
+    --ini-base ${d}/projects/srav1/creds \
+    --cluster-ini ~/.recount/cluster-knl.ini \
+    1
