f45391d29c7f1beaa7a5e8c113764af500d52896
Ben Langmead <langmea@cs.jhu.edu>
2018-12-05 18:30:16 -0500

    sort by srp

--- projects/srav1/make_meta.sh
+++ projects/srav1/make_meta.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl https://raw.githubusercontent.com/nellore/runs/master/sra/hg19.stats_by_sample.tsv | \
-    sed 's/^sample/#sample/' | \
-    awk '{print $2,$5}' > \
+    awk '{print $2,$5}' | \
+    sed 's/^index/#index/' |
+    sort -k1,1 > \
     srav1.txt
-
