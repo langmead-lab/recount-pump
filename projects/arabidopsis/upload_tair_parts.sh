6e756803620e64328d7ecbad0a44b4e8204b7a19
Ben Langmead <langmea@cs.jhu.edu>
2018-12-04 21:29:11 -0500

    add unmapped_star_idx

--- projects/arabidopsis/upload_tair_parts.sh
+++ projects/arabidopsis/upload_tair_parts.sh
@@ -2,6 +2,6 @@
 
 species=tair10araport11
 
-for i in star_idx.tar.gz gtf.tar.gz fasta.tar.gz kallisto_index.tar.gz salmon_index.tar.gz ; do
+for i in star_idx.tar.gz unmapped_star_idx.tar.gz gtf.tar.gz fasta.tar.gz kallisto_index.tar.gz salmon_index.tar.gz ; do
     aws --profile jhu-langmead s3 cp --acl public-read ${i} s3://recount-ref/${species}/${i}
 done
