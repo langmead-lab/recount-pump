70f27ea9d2d584e6d9a4ea413a7901bfe947bc78
Ben Langmead <langmea@cs.jhu.edu>
2019-01-16 12:16:21 -0500

    make easier to run w/ file argument

--- populate/disjoin_docker.sh
+++ populate/disjoin_docker.sh
@@ -3,15 +3,28 @@
 d=$(dirname $0)
 [[ ! -f disjoin.R ]] && cp $d/disjoin.R .
 
-url=$1
-shift
-[[ -z "${url}" ]] && echo "Specify URL as 1st argument" && exit 1
+if [[ $1 == "url" ]] ; then
+    shift
+    
+    url=$1
+    shift
+    [[ -z "${url}" ]] && echo "Specify URL as 1st argument" && exit 1
+    
+    name=$1
+    shift
+    [[ -z "${name}" ]] && echo "Specify name as 2nd argument" && exit 1
 
-name=$1
-shift
-[[ -z "${name}" ]] && echo "Specify name as 2nd argument" && exit 1
+    # Run from this directory
+    docker run -it \
+        -v `pwd`:/work bioconductor/release_core2 \
+        /work/disjoin.R ${url} /work/${name}.bed $*
+else
+    cp $1 .
+    name=$(basename $1)
+    shift
 
-# Run from this directory
-docker run -it \
-    -v `pwd`:/work bioconductor/release_core2 \
-    /work/disjoin.R ${url} /work/${name}.bed $*
+    # Run from this directory
+    docker run -it \
+        -v `pwd`:/work bioconductor/release_core2 \
+        /work/disjoin.R /work/${name} /work/${name}.bed $*
+fi
