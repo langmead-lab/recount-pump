b26aff85031322cebb5c7166a245c0c1b1fd70eb
Ben Langmead <langmea@cs.jhu.edu>
2019-01-17 14:11:21 -0500

    parameter to visibility timeout should be int

--- src/pump.py
+++ src/pump.py
@@ -201,13 +201,13 @@ def get_queue(sqs_client,
         make_dlq = True
     resp = sqs_client.create_queue(QueueName=queue_name,
                                    Attributes={
-                                       'VisibilityTimeout': str(visibility_timeout),
+                                       'VisibilityTimeout': visibility_timeout,
                                        'MessageRetentionPeriod': str(message_retention_period)})
     assert 'QueueUrl' in resp
     if make_dlq:
         dlq_resp = sqs_client.create_queue(QueueName=queue_name + '_dlq',
                                            Attributes={
-                                               'VisibilityTimeout': str(visibility_timeout),
+                                               'VisibilityTimeout': visibility_timeout,
                                                'MessageRetentionPeriod': str(message_retention_period)})
         assert 'QueueUrl' in dlq_resp
         response = sqs_client.get_queue_attributes(
