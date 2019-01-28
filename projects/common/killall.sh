b6ee238758aaf7f82875e500844d704a4dd857e0
Ben Langmead <langmea@cs.jhu.edu>
2019-01-28 12:10:13 -0500

    improved comment

--- projects/common/killall.sh
+++ projects/common/killall.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-squeue -l -u $USER | awk '{print $1}' | grep '[0-9]' | xargs scancel
+# Kills all analysis jobs (actually, all the user's jobs!) currently underway,
+# assuming Slurm
+
+# TODO: just kill recount-related jobs
+
+squeue -l -u ${USER} | awk '{print $1}' | grep '[0-9]' | xargs scancel
