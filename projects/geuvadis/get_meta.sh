3dfab1bcc51b1912d1e2ef86333c9584142f93d3
Ben Langmead <langmea@cs.jhu.edu>
2019-01-28 12:06:41 -0500

    fix underscore/dot issue

--- projects/geuvadis/get_meta.sh
+++ projects/geuvadis/get_meta.sh
@@ -6,7 +6,7 @@ set -ex
 
 get_metadata() {
     pushd $d/../../src
-    python -m metadata.sradbv2 search "study_accession:ERP001942" --gzip --output geuv.json
+    python -m metadata.sradbv2 search "study.accession:ERP001942" --gzip --output geuv.json
     popd
     mv $d/../../src/geuv.json.gz $d/
 }
