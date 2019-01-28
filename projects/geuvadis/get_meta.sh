6b8fda0b7f556e38eef9c3ed7c9fc8a24323e439
Ben Langmead <langmea@cs.jhu.edu>
2019-01-28 15:00:45 -0500

    parse study from ini

--- projects/geuvadis/get_meta.sh
+++ projects/geuvadis/get_meta.sh
@@ -2,13 +2,15 @@
 
 d=$(dirname $0)
 
+STUDY=$(grep '^study' $d/project.ini | cut -d"=" -f2 | tr -d '[:space:]')
+
 set -ex
 
 get_metadata() {
     pushd $d/../../src
-    python -m metadata.sradbv2 search "study.accession:ERP001942" --gzip --output geuv.json
+    python -m metadata.sradbv2 search "study.accession:ERP001942" --gzip --output ${STUDY}.json
     popd
-    mv $d/../../src/geuv.json.gz $d/
+    mv $d/../../src/${STUDY}.json.gz $d/
 }
 
 get_metadata
