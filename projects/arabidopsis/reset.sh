0c2a8f6c544f1c34cb057b73209678efc83509cb
Ben Langmead <langmea@cs.jhu.edu>
2019-01-29 19:56:51 -0500

    adapt arabidopsis project to new regime

--- projects/arabidopsis/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-../../aws/logs/delete.sh recount-tair
-../../aws/db/reset_db.sh recount_tair
-../../aws/sqs/delete.sh tair10araport11_proj1_q
--- projects/arabidopsis/upload_meta.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-study=tair10araport11
-
-aws --profile jhu-langmead s3 cp \
-    ${study}.json.gz \
-    s3://recount-pump-experiments/${study}/${study}.json.gz
