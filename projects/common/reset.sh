c3be6b304896688a456448ea5d9036ea5fb2cc29
Ben Langmead <langmea@cs.jhu.edu>
2019-02-04 20:44:46 -0500

    fix mistaken recount_ prefix

--- projects/common/reset.sh
+++ projects/common/reset.sh
@@ -4,7 +4,7 @@ set -ex
 
 STUDY=$(grep '^study' project.ini | cut -d"=" -f2 | tr -d '[:space:]')
 
-../../aws/logs/delete.sh recount_${STUDY}
-../../aws/db/reset_db.sh recount_${STUDY}
+../../aws/logs/delete.sh ${STUDY}
+../../aws/db/reset_db.sh ${STUDY}
 ../../aws/sqs/delete.sh ${STUDY}_proj1_q
 ../../aws/sqs/delete.sh ${STUDY}_proj1_q_dlq
