60cc3d4e6d034f4f46b2d4fa8f38571613e09d6f
Ben Langmead <langmea@cs.jhu.edu>
2019-02-06 12:20:03 -0500

    move some defunct scripts out of common into old

--- workflow/common/test.sh
+++ workflow/common/test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Lightweight test script, assuming you're in a conda or other environment
+# where all the required tools are installed
+
 snakemake -j2 --config \
     input=../common/accessions.txt \
     output=output \
