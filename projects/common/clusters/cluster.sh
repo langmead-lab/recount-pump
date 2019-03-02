91780376c6629d6a86a6ccf6f7171749782882ed
Ben Langmead <langmea@cs.jhu.edu>
2019-03-02 17:48:23 -0500

    detect when on bridges

--- projects/common/clusters/cluster.sh
+++ projects/common/clusters/cluster.sh
@@ -2,6 +2,8 @@
 
 if [[ -f /etc/motd ]] && grep -q Stampede2 /etc/motd ; then
     echo stampede2
+elif [[ -f /etc/motd ]] && grep -q 'bridges\.psc\.edu' /etc/motd ; then
+    echo bridges
 elif echo $(hostname) | grep -q bc-login ; then
     echo marcc
 elif [[ -f /etc/hosts ]] && grep -q hhpc /etc/hosts ; then
