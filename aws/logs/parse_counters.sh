972fc571d9a65c8e13ad436061172a84d37714f6
Ben Langmead <langmea@cs.jhu.edu>
2019-03-24 13:13:43 -0400

    update log scripts to use creds subdir

--- aws/logs/parse_counters.sh
+++ aws/logs/parse_counters.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Scrape all the counter names from all the relevant scripts.
+
 d=$(dirname $0)
 
 metrics=$(grep 'log.*COUNT_' $d/../../src/*.py | sed 's/.*COUNT/COUNT/' | sed 's/ .*//' | grep -v '%')
