aabd3b7f0355bf07a2dfc1d79ed8bcd36d456626
Ben Langmead <langmea@cs.jhu.edu>
2019-03-25 15:24:52 -0400

    update minio image to use precalculated text file instead of sradbv2

--- docker/minio/init_db.sh
+++ docker/minio/init_db.sh
@@ -25,14 +25,32 @@ setup_metadata() {
     QUERY="${QUERY} experiment_platform:illumina AND"
     QUERY="${QUERY} run_bases:[1 TO 75000000] AND"
     QUERY="${QUERY} run_FileDate:[\"2013-01-01\" TO \"2017-05-01\"]"
-    python -m metadata.sradbv2 search "${QUERY}" --gzip --output $STAGING/recount-meta/ce10_test/ce10_test.json
+    python -m metadata.sradbv2 search "${QUERY}" --gzip --output ${STAGING}/recount-meta/ce10_test/ce10_test.json
+}
+
+setup_metadata_stubbed() {
+    # because sradbv2 changed and I haven't caught up yet, we have to
+    # do this with a verbatim file for now
+
+    # Got this list by doing:
+    # aws --profile jhu-langmead s3 cp s3://recount-meta/ce10_test/ce10_small_test.json.gz - \
+    #    | gzip -dc | grep '"_id"' | sed 's/.*: "//' | sed 's/".*//'
+    mkdir -p $STAGING/recount-meta/ce10_test
+    cat >> ${STAGING}/recount-meta/ce10_test/ce10_test.txt <<EOF
+ce10test SRR4320662
+ce10test SRR4320703
+ce10test SRR4320751
+ce10test SRR2054443
+EOF
+    gzip -c ${STAGING}/recount-meta/ce10_test/ce10_test.txt > ${STAGING}/recount-meta/ce10_test/ce10_test.txt.gz
+    rm -f ${STAGING}/recount-meta/ce10_test/ce10_test.txt
 }
 
 setup_manifest /tmp/manifest.csv
 #setup_metadata
+setup_metadata_stubbed
 
 rm -rf /tmp/src
 
 echo "Final contents:"
 tree /tmp
-
