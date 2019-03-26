8e02c2bb3c027ad89dbdecda3c12b1caef54fc02
Ben Langmead <langmea@cs.jhu.edu>
2019-03-25 20:38:57 -0400

    move to double brackets and set -ex

--- aws/sqs/delete.sh
+++ aws/sqs/delete.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 INI_FILE="creds/queue.ini"
 if [[ ! -f ${INI_FILE} ]] ; then
     echo "Could not find creds/queue.ini"
@@ -11,23 +13,23 @@ profile=$(grep '^aws_profile' ${INI_FILE} | cut -d"=" -f2 | tr -d '[:space:]')
 region=$(grep '^region' ${INI_FILE} | cut -d"=" -f2 | tr -d '[:space:]')
 queue_name=stage_2
 
-if [ -n "$1" ] ; then
+if [[ -n "$1" ]] ; then
     queue_name=$1
 fi
 
 get_url() {
     aws --profile ${profile} \
-	--region ${region} \
-	sqs \
-	get-queue-url \
-	--queue-name "${queue_name}"
+        --region ${region} \
+        sqs \
+        get-queue-url \
+        --queue-name "${queue_name}"
 }
 
 url=$(get_url | grep QueueUrl | sed 's/^[^:]*://' | sed 's/"//g')
 
 echo "Got url: ${url}"
 
-if [ -z "${url}" ] ; then
+if [[ -z "${url}" ]] ; then
     echo "No URL" && exit 1
 fi
 
@@ -36,4 +38,3 @@ aws --profile ${profile} \
     sqs \
     delete-queue \
     --queue-url ${url}
-
