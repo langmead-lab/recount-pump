5e45987f0dcb6cfcba0dc64218a2a9b25924d704
Ben Langmead <langmea@cs.jhu.edu>
2019-03-26 20:23:33 -0400

    fix metadata filename

--- projects/ce10/check_buckets.sh
+++ projects/ce10/check_buckets.sh
@@ -3,7 +3,7 @@
 set -e
 
 which mc
-mc stat s3/recount-meta/ce10_test/ce10_small_test.json.gz
+mc stat s3/recount-meta/ce10_test/ce10_test.txt.gz
 mc stat s3/recount-ref/ce10/star_idx.tar.gz
 mc mb --ignore-existing s3/recount-output
 
