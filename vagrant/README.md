64cb3cad7c171af1ff1c3509f7f881b6ef021408
Ben Langmead <langmea@cs.jhu.edu>
2019-03-27 14:08:09 -0400

    Update README and Vagrantfiles for AWS-based scripts

--- vagrant/README.md
+++ vagrant/README.md
@@ -1,3 +1,11 @@
+Two of the subdirectories (`build_integration` and `build_workflow`) contain `Vagrantfile`s for AWS-based activities.  The third (`recount-dev`) contains a `Vagrantfile` for setting up a Debian-based development environment in which it's easy to run `recount-pump` and its tests.
+
+The AWS-based `Vagrantfile`s require some one-time setup.  Also, they should be invoked using the `vagrant_run.py` wrapper script.  That script helps by first setting certain AWS config-related environment variables, and by sending a Slack notification with a summary of how to run went. 
+
+The `aws.json` file contains relevant AWS settings and IDs that are then used in the `Vagrantfile`s.  The contents checked in to the repo are specific to the Langmead Lab AWS setup, so you will need to modify if you do not have access to our account.  (None of what's in that file is secret.)
+
+### AWS setup
+
 For EC2-based Vagrant setups (`build_integration` and `build_workflow`), some preparation is needed:
 
 * Create a VPC
@@ -12,3 +20,23 @@ For EC2-based Vagrant setups (`build_integration` and `build_workflow`), some pr
 * Associate a security group
 * Add the internet gateway to the route table for the VPC
     * It has to already be "associated with the VPC"
+    * Actions -> Edit Routes...
+
+To run either `build_integration` or `build_workflow`, change to the subdirectory and run `../vagrant_run.py`.  See documentation for `../vagrant_run.py` for how to (a) suppress the Slack message, (b) switch AWS profiles, etc. 
+
+### Setup `vagrant` for AWS
+
+Special `vagrant` plugins are needed for AWS support and for EC2 spot support in particular.
+
+```
+vagrant plugin install vagrant-aws-mkubenka --plugin-version "0.7.2.pre.22"
+vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+```
+
+### AWS troubleshooting
+
+* Vagrant hangs on `Waiting for SSH to become available...`
+    * Log into console and find record for EC2 instance
+        * Are public DNS and public IP blank?
+        * If so, double check that the subnets have "auto-assign public IP" box checked under Actions -> Modify auto-assign IP
+
--- vagrant/vagrant_run.py
+++ vagrant/vagrant_run.py
@@ -9,10 +9,12 @@ Usage:
   vagrant_run [options]
 
 Options:
+  --aws-json <path>          Path to file defining AWS-related variables [default: ../aws.json].
+  --aws-profile <path>       Path to file defining AWS-related variables [default: jhu_ue2].
   --skip-run                 Skip running Vagrant, just look at vagrant.log.
   --skip-slack               Don't send message to Slack.
   --slack-ini <ini>          ini file for Slack webhooks [default: ~/.recount/slack.ini].
-  --slack-section <section>  ini file section for log aggregator [default: slack].
+  --slack-section <section>  ini file load_aws_json()section for log aggregator [default: slack].
   -a, --aggregate            Enable log aggregation.
   -h, --help                 Show this screen.
   --version                  Show version.
@@ -28,6 +30,7 @@ secret=XXXXXXXXXXXXXXXXXXXXXXXX
 import os
 import sys
 import requests
+import json
 from docopt import docopt
 try:
     from configparser import RawConfigParser
@@ -50,7 +53,34 @@ def read_slack_config(ini_fn, section='slack'):
     return tstring, bstring, secret
 
 
-def run(skip_run, skip_slack, ini_fn, section):
+def load_aws_json(json_fn, profile):
+    js = json.loads(open(json_fn, 'rt').read())
+    if profile not in js['profile']:
+        raise RuntimeError('No such profile as "%s" in AWS config "%s"' % (profile, json_fn))
+    js_prof = js['profile'][profile]
+    region = js_prof['region']
+    subnet = js_prof['subnet'][js_prof['subnet'].keys()[0]]
+    security_group = js_prof['security_group']
+    ami = js['ec2']['ami'][region]
+    keypair = js_prof['keypair']
+    bid_price = js['ec2']['bid_price'][region]
+    instance_type = js['ec2']['instance_type']
+    aws_profile = js_prof['profile']
+    return region, subnet, security_group, ami, keypair, bid_price, instance_type, aws_profile
+
+
+def run(skip_run, skip_slack, ini_fn, section, aws_json, profile):
+    if not os.path.exists(aws_json):
+        raise RuntimeError('AWS json file "%s" does not exist' % aws_json)
+    region, subnet, security_group, ami, keypair, bid_price, instance_type, aws_profile = load_aws_json(aws_json, profile)
+    os.environ['VAGRANT_AWS_PROFILE'] = aws_profile
+    os.environ['VAGRANT_AWS_REGION'] = region
+    os.environ['VAGRANT_AWS_SUBNET_ID'] = subnet
+    os.environ['VAGRANT_AWS_SECURITY_GROUP'] = security_group
+    os.environ['VAGRANT_AWS_AMI'] = ami
+    os.environ['VAGRANT_AWS_EC2_KEYPAIR'] = keypair
+    os.environ['VAGRANT_AWS_EC2_INSTANCE_TYPE'] = instance_type
+    os.environ['VAGRANT_AWS_EC2_BID_PRICE'] = bid_price
     slack_url = slack_webhook_url(ini_fn, section=section)
     if not skip_run:
         os.system('vagrant up 2>&1 | tee vagrant.log')
@@ -79,4 +109,4 @@ def run(skip_run, skip_slack, ini_fn, section):
 if __name__ == '__main__':
     args = docopt(__doc__)
     slack_ini = os.path.expanduser(args['--slack-ini'])
-    run(args['--skip-run'], args['--skip-slack'], slack_ini, args['--slack-section'])
+    run(args['--skip-run'], args['--skip-slack'], slack_ini, args['--slack-section'], args['--aws-json'], args['--aws-profile'])
