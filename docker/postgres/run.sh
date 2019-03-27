61b9f78189178af03c216ac0a17011fb88753b60
Ben Langmead <langmea@cs.jhu.edu>
2019-03-27 16:28:44 -0400

    put recount-test db back -- it's needed by unit tests

--- docker/postgres/run.sh
+++ docker/postgres/run.sh
@@ -3,4 +3,5 @@
 docker run --rm --name postgres-recount \
     -e POSTGRES_USER=recount \
     -e POSTGRES_PASSWORD=recount-postgres \
+    -e POSTGRES_DB=recount-test \
     -p 25432:5432 -d postgres:10.4
