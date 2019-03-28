cbab02c04f8b6604eb994176404180972b49836a
Ben Langmead <langmea@cs.jhu.edu>
2019-03-28 14:39:08 -0400

    bulldoze /data dir; not sure why I have to, though

--- docker/minio/start_db.sh
+++ docker/minio/start_db.sh
@@ -5,13 +5,20 @@ set -ex
 [[ -z "${MINIO_ACCESS_KEY}" ]] && echo "MINIO_ACCESS_KEY must be set" && exit 1
 [[ -z "${MINIO_SECRET_KEY}" ]] && echo "MINIO_SECRET_KEY must be set" && exit 1
 
+# BTL: I don't quite understand why this is necessary.  Somehow the
+# files in /data are living across container runs.  Might have to do
+# with the VOLUME["/data"] directive in the base Dockerfile.
+rm -rf /data/*
 minio server /data &
 pid=$!
 /tmp/wait-for-it.sh localhost:9000
 
 sleep 2
-mv /tmp/staging/* /data/
+for i in /tmp/staging/* ; do
+    mv ${i} /data/
+done
 du -sh /data
 tree /data
 wait ${pid}
+
 echo "Finished waiting for minio server at pid=${pid}, all ready"
