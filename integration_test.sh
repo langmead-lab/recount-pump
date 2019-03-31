70fd93d4a2939b16213359535c9c194ded301057
Ben Langmead <langmea@cs.jhu.edu>
2019-03-30 20:33:51 -0400

    add some mc tests to integration_test.sh

--- integration_test.sh
+++ integration_test.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
+which mc
 docker-compose up -d
-sleep 15
+sleep 25
 
 export AWS_ACCESS_KEY_ID=minio
 export AWS_SECRET_ACCESS_KEY=minio123
@@ -14,6 +15,8 @@ export RECOUNT_TEST_S3="http://localhost:29000"
 # Needs to match cluster.ini
 export RECOUNT_IMAGES="${HOME}/recount-images"
 export SINGULARITY_CACHEDIR="${RECOUNT_IMAGES}"
+
+mc ls s3
 
 # Remove python and pytest caches
 ./clean.sh
