62082d2b700f26ad76583b4697b59613ca4bf403
Ben Langmead <langmea@cs.jhu.edu>
2019-04-11 08:25:46 -0400

    adapt the summarize script

--- alignment/summarize.py
+++ alignment/summarize.py
@@ -12,13 +12,23 @@ with open('accuracy.csv', 'wt') as ofh:
                fn == 'perform_mapping_accuracy_summary' or fn == 'perform_mapping_accuracy_SC_summary':
                 print("Found summary: %s/%s" % (dir, fn), file=sys.stderr)
                 dir_parts = dir.split('/')
-                expt, tool, samp = dir_parts[-1], dir_parts[-2], dir_parts[-3]
+                #expt, tool, samp = dir_parts[-1], dir_parts[-2], dir_parts[-3]
+                assert dir_parts[0] == 'output'
+                tool = 'star'
+                samp = dir_parts[1]
+                expt = dir_parts[2]
+                if expt.endswith('_male'):
+                    expt = expt[:-5]
+                if expt.endswith('_female'):
+                    expt = expt[:-7]
                 intron = 'T' if fn == 'perform_intron_recovery_summary' else 'F'
                 alignment = 'T' if 'mapping' in fn else 'F'
                 softclip = 'T' if 'accuracy_SC' in fn else 'F'
                 paired = 'T' if '_paired_' in expt else 'F'
-                ann = 'T' if expt.startswith('ann_') else 'F'
-                pass2 = 'T' if expt.endswith('_2pass') else 'F'
+                ann = 'F'
+                pass2 = 'F'
+                #ann = 'T' if expt.startswith('ann_') else 'F'
+                #pass2 = 'T' if expt.endswith('_2pass') else 'F'
                 with open(os.path.join(dir, fn), 'rt') as fh:
                     for ln in fh:
                         toks = ln.rstrip().split('\t')
@@ -32,21 +42,22 @@ with open('accuracy.csv', 'wt') as ofh:
                             rec = [samp, tool, paired, ann, pass2, intron, alignment, softclip, 'F'] + toks
                             ofh.write(','.join(rec) + '\n')
     
-           
-with open('times.csv', 'wt') as ofh:
-    for dir, subdirs, files in os.walk('output'):
-        for file in files:
-            if file.endswith('_times.log'):
-                print("Found timing results: %s/%s" % (dir, file), file=sys.stderr)
-                tool = file.split('_')[0]
-                samp = file[len(tool)+1:-len('_times.log')]
-                recs = []
-                with open(os.path.join(dir, file), 'rt') as fh:
-                    for ln in fh:
-                        if len(ln.strip()) == 0:
-                            continue
-                        if ln.split()[0] == 'real':
-                            time = ln.rstrip().split()[-1]
-                            recs.append(time)
-                if len(recs) == 4:
-                    ofh.write(','.join([samp, tool] + recs) + '\n')
+
+# with open('times.csv', 'wt') as ofh:
+#     for dir, subdirs, files in os.walk('output'):
+#         for file in files:
+#             if file.endswith('_times.log'):
+#                 print("Found timing results: %s/%s" % (dir, file), file=sys.stderr)
+#                 #tool = file.split('_')[0]
+#                 tool = 'star'
+#                 samp = file[len(tool)+1:-len('_times.log')]
+#                 recs = []
+#                 with open(os.path.join(dir, file), 'rt') as fh:
+#                     for ln in fh:
+#                         if len(ln.strip()) == 0:
+#                             continue
+#                         if ln.split()[0] == 'real':
+#                             time = ln.rstrip().split()[-1]
+#                             recs.append(time)
+#                 if len(recs) == 4:
+#                     ofh.write(','.join([samp, tool] + recs) + '\n')
