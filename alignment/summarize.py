6b7c4d763e4185591437594d88d93cccad403297
Ben Langmead <ben.langmead@gmail.com>
2019-04-18 08:21:44 -0400

    fix summarize

--- alignment/summarize.py
+++ alignment/summarize.py
@@ -5,11 +5,11 @@ import os
 import sys
 
 with open('accuracy.csv', 'wt') as ofh:
-    ofh.write('samp,tool,paired,ann,twopass,inton,alignment,softclip,baselev,measure,value\n')
+    ofh.write('samp,tool,expt,paired,ann,twopass,inton,alignment,softclip,baselev,measure,value\n')
     for dir, subdirs, files in os.walk('output'):
         for fn in files:
-            if fn == 'perform_summary' or fn == 'perform_intron_recovery_summary' or \
-               fn == 'perform_mapping_accuracy_summary' or fn == 'perform_mapping_accuracy_SC_summary':
+            if fn == 'perf_summary' or fn == 'perf_intron_recovery_summary' or \
+               fn == 'perf_mapping_accuracy_summary' or fn == 'perf_mapping_accuracy_SC_summary':
                 print("Found summary: %s/%s" % (dir, fn), file=sys.stderr)
                 dir_parts = dir.split('/')
                 #expt, tool, samp = dir_parts[-1], dir_parts[-2], dir_parts[-3]
@@ -21,7 +21,7 @@ with open('accuracy.csv', 'wt') as ofh:
                     expt = expt[:-5]
                 if expt.endswith('_female'):
                     expt = expt[:-7]
-                intron = 'T' if fn == 'perform_intron_recovery_summary' else 'F'
+                intron = 'T' if fn == 'perf_intron_recovery_summary' else 'F'
                 alignment = 'T' if 'mapping' in fn else 'F'
                 softclip = 'T' if 'accuracy_SC' in fn else 'F'
                 paired = 'T' if '_paired_' in expt else 'F'
@@ -34,12 +34,12 @@ with open('accuracy.csv', 'wt') as ofh:
                         toks = ln.rstrip().split('\t')
                         assert len(toks) == 2 or len(toks) == 3, (dir, fn, str(toks))
                         if len(toks) == 3:
-                            rec1 = [samp, tool, paired, ann, pass2, intron, alignment, softclip, 'T', toks[0], toks[1]]
-                            rec2 = [samp, tool, paired, ann, pass2, intron, alignment, softclip, 'F', toks[0], toks[2]]
+                            rec1 = [samp, tool, expt, paired, ann, pass2, intron, alignment, softclip, 'T', toks[0], toks[1]]
+                            rec2 = [samp, tool, expt, paired, ann, pass2, intron, alignment, softclip, 'F', toks[0], toks[2]]
                             ofh.write(','.join(rec1) + '\n')
                             ofh.write(','.join(rec2) + '\n')
                         else:
-                            rec = [samp, tool, paired, ann, pass2, intron, alignment, softclip, 'F'] + toks
+                            rec = [samp, tool, expt, paired, ann, pass2, intron, alignment, softclip, 'F'] + toks
                             ofh.write(','.join(rec) + '\n')
     
 
