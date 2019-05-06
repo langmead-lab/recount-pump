8896851f42ed5f5823933e9b506491272c23fe3d
ChristopherWilks <broadsword@gmail.com>
2019-05-06 16:19:14 -0400

    fixed sra query, but didnt make a difference

--- metadata/fetch_sra_metadata.py
+++ metadata/fetch_sra_metadata.py
@@ -1,25 +1,27 @@
 #script to write out jobs to download all of SRA human & mouse RNA-seq metadata
 import sys
 from Bio import Entrez as e
-xml_ver_patt=re.compile(r'<.*xml\s+version=.+>')
 #required to set an email
 e.email="downloadsRUs@dos.com"
 #capture 1) Illumina 2) RNA-Seq 3) human 4) transriptome sources 5) public access (non-dbgap)
 #for scRNA maybe use "TRANSCRIPTOMIC SINGLE CELL" as Source argument instead of transcriptomic?
-base_query = '((((illumina[Platform]) AND rna seq[Strategy]) AND transcriptomic[Source]) AND public[Access]'
+#base_query = '((((illumina[Platform]) AND rna seq[Strategy]) AND transcriptomic[Source]) AND public[Access])'
+base_query = '((((illumina[Platform]) AND rna seq[Strategy]) AND transcriptomic single cell[Source]) AND public[Access])'
 #prepend "[Organism]" to each of these in the search text
 organisms=['human','mouse']
+if len(sys.argv) > 1:
+    organisms = [sys.argv[1]]
 
 #max number of records allowed to be fetched at once using EFetch
 #return_max=1000
 return_max=1000
 
 for orgn in organisms:
-    es_ = e.esearch(db="sra",retmax=1,term=base_query+" %s[Organism]" % orgn,usehistory=True)
+    es_ = e.esearch(db="sra",retmax=1,term=base_query+" AND %s[Organism]" % orgn,usehistory=True)
     es = e.read(es_)
     #number of records is # of EXPERIMENTs (SRX) NOT # RUNs (SRR)
     total_records = int(es["Count"])
-    sys.stderr.write("Total # of records is %d for %s using query %s\n" % (total_records, orgn, base_query+" %s[Organism]" % orgn))
+    sys.stderr.write("Total # of records is %d for %s using query %s\n" % (total_records, orgn, base_query+" and %s[Organism]" % orgn))
     num_fetches = int(total_records / return_max) + 1
     for retstart_idx in range(0,num_fetches):
         start_idx = retstart_idx * return_max
