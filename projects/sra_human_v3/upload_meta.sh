01f045f5bdd0c0c4951cd4631ee9feab9c4324fc
ChristopherWilks <broadsword@gmail.com>
2019-05-13 14:41:36 -0400

    added upload of TSV of all SRA metadata

--- projects/sra_human_v3/upload_meta.sh
+++ projects/sra_human_v3/upload_meta.sh
@@ -13,3 +13,5 @@ done
 
 #upload the original XMLs from the SRA search output
 aws --profile jhu-langmead s3 cp all_human_9606_rnaseq_transcriptomic_illumina_public.raw_xmls.20190503.tar.gz s3://recount-pump-experiments/${superstudy}/
+#upload the converted to TSV version of the metadata from the raw XMLs in the previous file
+aws --profile jhu-langmead s3 cp all_human_9606_rnaseq_transcriptomic_illumina_public.converted_from_xml.20190503.tsv.gz s3://recount-pump-experiments/${superstudy}/
