08f4e5b1d8694018dac5b9b2695470dd33b802dc
ChristopherWilks <broadsword@gmail.com>
2020-04-13 19:23:18 -0400

    support multiple organisms in sra md pipeline

--- metadata/metadata_pipeline.sh
+++ metadata/metadata_pipeline.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 #runs the full metadata fetching & parsing pipeline for SRA
 
-#submit an organism (e.g. "human" or "mouse")
+#submit an organism (e.g. "${orgn}" or "mouse")
 orgn=$1
 #number of concurrent fetch/filter jobs to run
 #probably not more than 10, and more like 5
 num_procs=$2
+
+orgn_orig=$orgn
+orgn=`echo -n "$orgn_orig" | perl -ne '$o=$_; $o=~s/ /_/g; print "$o";'`
 
 #python needs to have BioPython installed as it uses Entrez from Bio
 #fetch jobs go to fetch_${orgn}.jobs
 #parse jobs go to parse_${orgn}.sh (run serially)
 mkdir -p sra_xmls
 mkdir -p parse_errs
-python fetch_sra_metadata.py $orgn sra_xmls parse_errs
+python fetch_sra_metadata.py "$orgn_orig" $orgn sra_xmls parse_errs
 
-parallel -j $num_procs < fetch_${orgn}.jobs > fetch_${orgn}.jobs.run 2>&1
+#parallel -j $num_procs < fetch_${orgn}.jobs > fetch_${orgn}.jobs.run 2>&1
+parallel -j 3 < fetch_${orgn}.jobs > fetch_${orgn}.jobs.run 2>&1
+#/usr/bin/time -v /bin/bash -x fetch_${orgn}.jobs > fetch_${orgn}.jobs.run 2>&1
 
 #don't need to run parsing in parallel (fast enough) and we're writing to the same file
 #output file is: all_[orgn]_sra.tsv
@@ -30,12 +35,12 @@ mkdir nontranscriptomic
 pushd nontranscriptomic
 mkdir sra_xmls
 mkdir parse_errs
-python ../fetch_sra_metadata.py human sra_xmls parse_errs 1
-/bin/bash -x fetch_human.jobs
-/bin/bash -x parse_human.sh
-cut -f 2 all_human_sra.tsv | sort -u | sed -e 's/$/\t/' > all_human_sra.tsv.studies
-fgrep -f all_human_sra.tsv.studies ../all_human_sra.no_scrna_except_smart.full_studies.tsv | cut -f 2 | sed -e 's/$/\t/' | sort -u > studies_found_in_transcriptomic
-fgrep -f studies_found_in_transcriptomic all_human_sra.tsv > all_human_sra.tsv.in_transcriptomic
-cat ../all_human_sra.no_scrna_except_smart.full_studies.tsv all_human_sra.tsv.in_transcriptomic | sort -u > ../all_human_sra.no_scrna_except_smart.full_studies_with_nontranscriptomic_runs.tsv
+python ../fetch_sra_metadata.py "$orgn_orig" $orgn sra_xmls parse_errs 1
+/bin/bash -x fetch_${orgn}.jobs
+/bin/bash -x parse_${orgn}.sh
+cut -f 2 all_${orgn}_sra.tsv | sort -u | sed -e 's/$/\t/' > all_${orgn}_sra.tsv.studies
+fgrep -f all_${orgn}_sra.tsv.studies ../all_${orgn}_sra.no_scrna_except_smart.full_studies.tsv | cut -f 2 | sed -e 's/$/\t/' | sort -u > studies_found_in_transcriptomic
+fgrep -f studies_found_in_transcriptomic all_${orgn}_sra.tsv > all_${orgn}_sra.tsv.in_transcriptomic
+cat ../all_${orgn}_sra.no_scrna_except_smart.full_studies.tsv all_${orgn}_sra.tsv.in_transcriptomic | sort -u > ../all_${orgn}_sra.no_scrna_except_smart.full_studies_with_nontranscriptomic_runs.tsv
 popd
-ln -fs all_human_sra.no_scrna_except_smart.full_studies_with_nontranscriptomic_runs.tsv ALL_BULK_AND_SMART_SEQ.runs.tsv
+ln -fs all_${orgn}_sra.no_scrna_except_smart.full_studies_with_nontranscriptomic_runs.tsv all.runs.tsv
