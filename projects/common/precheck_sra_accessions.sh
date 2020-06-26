648564e720834c11b1e1f286e61c96d7a10e4a0c
ChristopherWilks <broadsword@gmail.com>
2020-06-26 09:33:38 -0500

    use var for output dir in precheck script

--- projects/common/precheck_sra_accessions.sh
+++ projects/common/precheck_sra_accessions.sh
@@ -3,11 +3,14 @@ list_file=$1
 #number of parallel processes to download 1 read FASTQs
 num_procs=$2
 
+#OUT='/dev/shm'
+OUT='./'
+
 export LC_ALL=C
-rm -rf /dev/shm/prechecks*
-mkdir -p /dev/shm/prechecks
-mkdir -p /dev/shm/prechecks.stdout
-mkdir -p /dev/shm/prechecks.stderr
-cut -d' ' -f 2 $list_file | sort -u | perl -ne 'chomp; print "fastq-dump -N 1 -X 1 -O /dev/shm/prechecks $_ > /dev/shm/prechecks.stdout/$_ 2> /dev/shm/prechecks.stderr/$_\n";' > runs.precheck
+rm -rf $OUT/prechecks*
+mkdir -p $OUT/prechecks
+mkdir -p $OUT/prechecks.stdout
+mkdir -p $OUT/prechecks.stderr
+cut -d' ' -f 2 $list_file | sort -u | perl -ne 'chomp; print "fastq-dump -N 1 -X 1 -O '$OUT'/prechecks $_ > '$OUT'/prechecks.stdout/$_ 2> '$OUT'/prechecks.stderr/$_\n";' > runs.precheck
 
 parallel -j $num_procs < runs.precheck
