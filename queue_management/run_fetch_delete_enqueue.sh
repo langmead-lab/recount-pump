df3b8189bc73874a100d1b6b519caf0bd5467308
ChristopherWilks <broadsword@gmail.com>
2020-10-20 14:08:20 -0400

    updates to queue manager script

--- queue_management/run_fetch_delete_enqueue.sh
+++ queue_management/run_fetch_delete_enqueue.sh
@@ -2,15 +2,20 @@ q=$1
 dlq="${q}_dlq"
 echo $dlq
 
+rm -rf queue-messages
+
 key=`fgrep aws_access_key_id ./credentials | head -1 | cut -d'=' -f 2 | sed -e 's/ //g'`
 echo $key
 secret=`fgrep aws_secret_access_key ./credentials | head -1 | cut -d'=' -f 2 | sed -e 's/ //g'`
 echo $secret
 
 #fetch AND delete from queue
-python2 fetch_all_dlq_messages.py -d -q $dlq -r us-east-2 -k $key -s $secret > fetch.${dlq}
+/data7/miniconda2/bin/python2 fetch_all_dlq_messages.py -d -q $dlq -r us-east-2 -k $key -s "$secret" > fetch.${dlq}
+#/data7/miniconda2/bin/python2 fetch_all_dlq_messages.py -d -q $q -r us-east-2 -k $key -s "$secret" > fetch.${q}
 
 cat queue-messages/* | fgrep "body" | cut -d'"' -f 4 > ${q}.all_strings.txt
+#cat queue-messages/* | fgrep "body" | cut -d'"' -f 4 > $dlq.leftovers.txt
+#cat queue-messages/* | fgrep "body" | cut -d'"' -f 4 > $q.leftovers.txt
 
 #now re-enqueue
-python2 enqueue_job_messages.py -q $q -f ${q}.all_strings.txt -r us-east-2 -k $key -s $secret  2> enqueue.${q}
+/data7/miniconda2/bin/python2 enqueue_job_messages.py -q $q -f ${q}.all_strings.txt -r us-east-2 -k $key -s "$secret"  2> enqueue.${q}
