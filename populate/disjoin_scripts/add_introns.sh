47a30c9d94d404436cb710f30a984785d8adda31
ChristopherWilks <broadsword@gmail.com>
2022-01-20 12:23:18 -0500

    adding missing scripts

---
+++ populate/disjoin_scripts/add_introns.sh
@@ -0,0 +1,19 @@
+#!/usr//bin/bash
+set -o pipefail -o nounset -o errexit 
+
+dir=$(dirname $0)
+
+GTF=$1
+EXON_BED=$2
+
+python2 $dir/extract_splice_sites.py ${GTF} > ${GTF}.introns
+
+#filter out non-autosomal chromosomes; convert to BED file; sort
+egrep -e '^chr' ${GTF}.introns | fgrep -v "chrUn" | sort -k1,1 -k2,2n -k3,3n | perl -ne 'chomp; ($c,$s,$e,$o)=split(/\t/,$_); $s--; print "$c\t$s\t$e\t.\t1\t$o\n";' > ${GTF}.introns.filtered_sorted.bed
+
+#remove any exon segments so we get consituitive introns (and partials)
+bedtools subtract -a ${GTF}.introns.filtered_sorted.bed -b $EXON_BED > ${GTF}.introns.filtered_sorted.exons_removed.bed
+
+sort -k1,1 -k2,2n -k3,3n ${GTF}.introns.filtered_sorted.exons_removed.bed | perl -ne 'chomp; $f=$_; ($c,$s,$e,$n,$t,$o)=split(/\t/,$f); if($pc) { if($c eq $pc && $s < $pe) { $pe=$e if($e > $pe); next; } print "$pc\t$ps\t$pe\t$p\n"; } $pc=$c; $ps=$s; $pe=$e; $p=join("\t",($n,$t,$o)); END { if($pc) { print "$pc\t$ps\t$pe\t$p\n"; }}' > ${GTF}.introns.filtered_sorted.exons_removed.bed.overlaps
+
+cat ${GTF}.introns.filtered_sorted.exons_removed.bed.overlaps $EXON_BED | sort -k1,1 -k2,2n -k3,3n > ${EXON_BED}.w_introns.sorted.bed
---
+++ populate/disjoin_scripts/run_post_disjoin_steps.sh
@@ -0,0 +1,42 @@
+dir=$(dirname $0)
+ORIG_UNIONED_GTF=$1
+#the previous script will copy the original input file into the current dir
+#and will output the results to the current dir with a bed suffix regardless of the actual file format
+ORIG_UNIONED_GTF=$(basename $ORIG_UNIONED_GTF)
+mv ${ORIG_UNIONED_GTF}.bed ${ORIG_UNIONED_GTF}.gff
+
+#extract the disjoint exons with gene IDs as a BED file, ${ORIG_UNIONED_GTF}.bed.gff is produced by the disjoin.R script
+fgrep -v "##" ${ORIG_UNIONED_GTF}.gff | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); $f[3]--; print "".join("\t",@f)."\n";'  | sort -t'	' -k1,1 -k4,4n -k5,5n > ${ORIG_UNIONED_GTF}.gff.bed.sorted
+
+##########Gene Mapping
+
+#get gene coords
+#this gets the largest coordinate range for each gene w/o resorting to subselecting by type (since for certain single exon genes that can be nuanced)
+#i.e. if "gene" and/or "exon" are used as type filters, some genes/exons will be missed
+cut -f 1,4,5,7,9 ${ORIG_UNIONED_GTF} | perl -ne 'chomp; ($c,$s,$e,$o,$ginfo)=split(/\t/,$_); $s--; $ginfo=~/gene_id\s+"([^"]+)/; $g=$1; if($h{$g} && $h{$g}->[2] ne $c) { $h{$g}->[4]=1; $g.=".$c"; } $h{$g}->[0]=$s if(!defined($h{$g}->[0]) || $h{$g}->[0] > $s); $h{$g}->[1]=$e if(!defined($h{$g}->[1]) || $h{$g}->[1] < $e); $h{$g}->[2]=$c; $h{$g}->[3]=$o; END { for $g (keys %h) { @a=@{$h{$g}}; if(scalar(@a) > 4) { $g.=".".$a[2]; } print "".join("\t",$a[2],$a[0],$a[1],$g,"0",$a[3],"2")."\n";}}' > ${ORIG_UNIONED_GTF}.gene_coords.bed
+
+#now produce the disjoint exon2annotated gene mapping file for rejoining to get annotated gene counts from disjoint exon counts
+cat ${ORIG_UNIONED_GTF}.gene_coords.bed ${ORIG_UNIONED_GTF}.gff.bed.sorted | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); $gid=$f[3]; if($f[6] eq "2") { $h{$gid}=$f; next; } $f=join("\t",@f); ($c,$s,$e,$o,$info)=($f[0],$f[3],$f[4],$f[6],$f[8]); $info=~/gene_id=([^;]+)/; $gs=$1; $info=~/exon_name=([^;]+)/; $es=$1; @genes=split(/,/,$gs); for $g (@genes) { $ginfo=$h{$g}; if(!$ginfo) { $ginfo=$h{$g.".$c"}; } print "$c\t$s\t$e\t.\t0\t$o\t$ginfo\n"; }' | sort -t'	' -k1,1 -k2,2n -k3,3n > ${ORIG_UNIONED_GTF}.disjoint2exons2genes.bed
+
+#calculate the per gene (in a specific annotation) base pair length (counting only disjoint exon lengths)
+cat ${ORIG_UNIONED_GTF}.disjoint2exons2genes.bed | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); ($s,$e,$g)=($f[1],$f[2],$f[9]); $h{$g}+=($e-$s); END { for $g_ (keys %h) { print "$g_\t".$h{$g_}."\n"; }}' | sort -k1,1 > ${ORIG_UNIONED_GTF}.disjoint2exons2genes.bed.gene_bpl.tsv
+
+
+##########Exon Mapping
+
+#get exon coords, this time we have to select by exons
+#BE VERY CAREFUL that the input GTF has a clear "exon" type label for every exon you have in your disjoint set!
+#this is true for G029.G026.R109.F006.20190220.gtf, but isn't for gencodev25 (recount2)
+
+#get disjoint2annotated exon mapping, assumes that the original unioned GTF file used the full pattern (exon/gene_name|chr|start|end|strand) as the exon_id
+#we'll use this to get the proper mapping back
+cat ${ORIG_UNIONED_GTF}.gff.bed.sorted | perl -ne 'chomp; $f=$_; @f=split(/\t/,$f); $eid=$f[3]; if($f[6] eq "2") { $h{$eid}=$f; next; } $f=join("\t",@f); ($c,$s,$e,$o,$info)=($f[0],$f[3],$f[4],$f[6],$f[8]); $info=~/gene_id=([^;]+)/; $gs=$1; $info=~/exon_name=([^;]+)/; $es=$1; @genes=split(/,/,$gs); @exons=@genes; if($es) { @exons=split(/,/,$es); } for $ex (@exons) { ($ename,$c2,$s2,$e2,$o2)=split(/\|/,$ex); $s2--; print "$c\t$s\t$e\t.\t0\t$o\t$c2\t$s2\t$e2\t$ename\t0\t$o2\t2\n"; }' 2> ${ORIG_UNIONED_GTF}.disjoint2exons.bed.err | sort -t'	' -k1,1 -k2,2n -k3,3n > ${ORIG_UNIONED_GTF}.disjoint2exons.bed
+
+#get exonID2geneIDs mapping
+cat ${ORIG_UNIONED_GTF}.disjoint2exons.bed | perl -ne 'BEGIN { open(IN,"<'${ORIG_UNIONED_GTF}'.disjoint2exons2genes.bed"); while($line=<IN>) { chomp($line); @f=split(/\t/,$line); ($c,$s,$e)=@f; $o=$f[5]; $k=join("\t",($c,$s,$e,$o)); $h{$k}.=$f[9].";"; } close(IN); } chomp; @f=split(/\t/,$_); ($c,$s,$e)=@f; $o=$f[5]; $k=join("\t",($c,$s,$e,$o)); $gids=$h{$k}; $h2{$f[9]}.="$gids"; END { for $e (keys %h2) { print "$e\t".$h2{$e}."\n";}}' > ${ORIG_UNIONED_GTF}.exonIDs2geneIDs.tsv
+
+#now get the exons.bed file to be used in the actual pipeline
+cut -f 1-6 ${ORIG_UNIONED_GTF}.disjoint2exons.bed | sort -k1,1 -k2,2n -k3,3n | uniq > ${ORIG_UNIONED_GTF}.disjoint_exons.bed
+
+#now add non-exon-overlapping introns for QC/stats
+/bin/bash -x $dir/add_introns.sh ${ORIG_UNIONED_GTF} ${ORIG_UNIONED_GTF}.disjoint_exons.bed
