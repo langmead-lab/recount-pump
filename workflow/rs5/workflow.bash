db4fe691a9815f6201f4dd39e878e47b3b32bdff
btrx-cwilks <cwilks@rbneuro.com>
2022-01-27 17:52:54 +0000

    bumping to ver 1.0.9 on pump image, change to broadsword quay repo, remove aspera startup script call, allow for Snakemake config file to be set externally via env var

--- workflow/rs5/workflow.bash
+++ workflow/rs5/workflow.bash
@@ -3,10 +3,47 @@
 # Author: Ben Langmead
 #  Email: langmea@cs.jhu.edu
 #   Date: 1/5/2018
+# Updates: Chris Wilks
+#  Email: broadsword@gmail.com
+#  Latest Date: 1/27/2022
 
 # Designed to run the nextflow script from inside a container, where relevant
 # directories are indicated by environment variables RECOUNT_INPUT,
 # RECOUNT_OUTPUT, RECOUNT_REF, RECOUNT_TEMP
+
+# for dbGaP downloads VDB_CONFIG may need to be set with the path to the user settings for sratoolkit,
+# e.g. VDB_CONFIG="/container-mounts/.ncbi/user-settings.mkfg"
+# also for dbGaP downloads, if using sratoolkit <=2.9.x, need to have the RECOUNT_TEMP_BIG and RECOUNT_TEMP under the configured
+# path in the VDB_CONFIG for the dbGaP project that is *reachable within the container*!
+# If using newer versions of sratoolkit, must pass the path to the NGC key in as the GDC token, again path should be reachable within
+# the container
+
+# for overriding elements of the Snakemake config (e.g. the path to the download script for instance)
+# define CONFIGFILE="/path/to/config.json" on a container reachable path
+# possible config params:
+
+# download_exe
+# bamcount_exe
+# temp
+# temp_big
+# input (input file string)
+# output (final output path)
+# keep_bam
+# keep_fastq
+# keep_unmapped_fastq
+# ref (path to overall reference location (e.g. parent dir of "hg38")
+# bw_bed (default exons.bed)
+# unique_qual (default 10)
+# featureCounts (extra params for FC, default '')
+# fc_unique_qual (default 10)
+# salmon_args (extra params for salmon, default '')
+# star_args (extra params for star, default '')
+# star_no_shared (dont use shared mem mode, default '0' i.e. use shared mem)
+# fastq_dump_args (extra params for fastq_dump, default '')
+# fastq_dump_retries (default 2)
+# prefetch_args (default '--max-size 200G -L info')
+# hisat2_args (extra params for hisat2 unaligned run, default '')
+# max_unalign (limit on number of unaligned to primary ref [hg38...] read/read pairs to save, default 100000)
 
 set -ex
 
@@ -67,9 +104,17 @@ fi
 if [[ -f /Snakefile ]] ; then
     mkdir -p ${RECOUNT_TEMP_BIG}/snakemake-wd
     pushd ${RECOUNT_TEMP_BIG}/snakemake-wd
-    CONFIGFILE=""
-    if [[ -f "${RECOUNT_TEMP_BIG}/config.json" ]] ; then
+    #CONFIGFILE should be set to container reachable path to a json file
+    if [[ -n $CONFIGFILE ]]; then
+        CONFIGFILE="--configfile $CONFIGFILE"
+    fi
+    #if CONFIGFILE not set but a config.json file is in the TEMP_BIG path already, use that
+    if [[ -z $CONFIGFILE && -f "${RECOUNT_TEMP_BIG}/config.json" ]] ; then
         CONFIGFILE="--configfile ${RECOUNT_TEMP_BIG}/config.json"
+    fi
+    #otherwise set to empty string
+    if [[ -z $CONFIGFILE ]]; then
+        CONFIGFILE=""
     fi
     snakemake \
         --snakefile /Snakefile \
