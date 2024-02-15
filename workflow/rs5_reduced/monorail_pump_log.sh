58f628e68f7c4f742c46e48d58b190b08cb0833c
btrx-cwilks <chris.wilks@neumoratx.com>
2024-02-15 16:22:50 +0000

    adding monorail pump logger script

---
+++ workflow/rs5_reduced/monorail_pump_log.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#set -exo pipefail
+export MD_IP=169.254.169.254
+run=$1
+study=$2
+#checkpoint OR path2fastqfiles,
+#checkpoint is one of:
+#+) START
+#+) STAR_DONE
+#+) INDEX_DONE
+#+) BAMCOUNT_DONE
+#+) END
+#if path2fastqfiles, files are checked for a  small sample of reads to see if there's evidence of true scRNA (chromium/10x/droplet)
+#also paired status and read length in both reads1 and reads2 files are logged
+mode=$3
+
+#need to establish a unique ID for this instance of processing this RUN accession through pump:
+#date.run_acc.study_acc.node_ip
+DATE=$(date +"%Y%m%dT%H%M%S%z")
+IP=$(curl http://${MD_IP}/latest/meta-data/local-ipv4)
+itype=$(curl http://${MD_IP}/latest/meta-data/instance-type)
+IP=$(echo "$IP" | sed 's#\.#-#g')
+JOB_ID="${run}.${study}.${IP}"
+
+#TODO2:
+#make 1 call to get: a) IP address annd b) instance type at init, then write to /dev/shm and have all workers read from that
+#then every message will have ID, itype, mode, cores, memory, disk space, load average
+
+rdir=$(dirname $mode)
+#fastqfiles mode:
+if [[ $rdir =~ "/" ]]; then
+    echo "logging FASTQ stats"
+    exit 0
+fi
+
+#collect additional stats from machine
+#disk usage
+df=$(df -h | tr -s " " $'|' | cut -d'|' -f 4,5,6 | tail -n+2 | fgrep -v "|/snap/" | fgrep -v "|/run" | fgrep -v "|/sys" | fgrep -v "|/dev" | tr $'\n' ";" | sed 's#;$#\n#')
+ncores=$(fgrep -i processor /proc/cpuinfo  | wc -l) 
+ram=$(head -3 /proc/meminfo | tr $'\n' ";" | sed 's# ##g' | sed 's#;$#\n#')
+load_avg=$(top -b -n  1  | awk '/load average/ { printf "%s\n", $12 }' | sed 's#,##')
+
+#now log:
+entry="${DATE};${mode};${JOB_ID};${itype};${ncores};${load_avg};${ram};${df}"
+echo "$entry"
