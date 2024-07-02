fdf5014e4eaf5792f461e3e983162b14dcad9a2d
btrx-cwilks <chris.wilks@neumoratx.com>
2024-07-02 18:31:23 +0000

    basic paths setup for running pump within container

---
+++ workflow/rs5_reduced/setup_local_monorail_links.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo ln -fs /shared-data/research/genomics/software/monorail/recount-pump/workflow/rs5_reduced/Snakefile /Snakefile
+sudo ln -fs /shared-data/research/genomics/software/monorail/recount-pump/workflow/rs5_reduced/download.sh /download.sh
+sudo ln -fs /home/ubuntu/bin /monorail_bin
+sudo ln -fs /home/ubuntu/bin /bamcount
