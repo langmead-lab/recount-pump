d11e58d9a9838d7206c180f0aedc62cef9f49cb3
btrx-cwilks <chris.wilks@neumoratx.com>
2024-08-05 18:49:26 +0000

    update s3 urls, use aws instead of curl, dont do STAR load yet

--- workflow/rs5_reduced/get_ref_indexes_fast.sh
+++ workflow/rs5_reduced/get_ref_indexes_fast.sh
@@ -9,7 +9,9 @@ mkdir -p $ref
 pushd $ref
 mkdir -p star_idx
 #retrieve split up star index files in parallel
-prefix="https://recount-ref.s3.amazonaws.com/$ref"
+#prefix="https://recount-ref.s3.amazonaws.com/$ref"
+prefix="https://genome-idx.s3.amazonaws.com/recount/recount-ref/$ref"
+s3prefix="s3://genome-idx/recount/recount-ref/$ref"
 
 for i in {0..19}; do i0=0; if [[ $i -gt 9 ]]; then i0=""; fi ; echo "aws s3 cp s3://monorail-batch/faster_refs/$ref/star_idx/SA${i0}${i} ./star_idx/"; done > s3dn.jobs
 for i in {0..2}; do echo "aws s3 cp s3://monorail-batch/faster_refs/$ref/star_idx/Genome0${i} ./star_idx/"; done >> s3dn.jobs
@@ -18,8 +20,8 @@ echo "aws s3 cp s3://monorail-batch/faster_refs/$ref/star_idx/SAindex ./star_idx
 echo "cat SA?? > SA" > cat.jobs
 echo "cat Genome?? > Genome" >> cat.jobs
 
-echo "curl $prefix/gtf.tar.gz 2> gtf.run | tar -zxvf - 2>> gtf.run" > detar.jobs
-echo "curl $prefix/fasta.tar.gz 2> fasta.run | tar -zxvf - 2>> fasta.run" >> detar.jobs
+echo "aws s3 cp $s3prefix/gtf.tar.gz - 2> gtf.run | tar -zxvf - 2>> gtf.run" > detar.jobs
+echo "aws s3 cp $s3prefix/fasta.tar.gz - 2> fasta.run | tar -zxvf - 2>> fasta.run" >> detar.jobs
 
 threads=$(cat s3dn.jobs | wc -l)
 #export threads=14
@@ -28,11 +30,13 @@ echo "pushd star_idx" >> s3dn.sh
 echo "/usr/bin/time -v parallel -j2 < ../cat.jobs > ../cat.jobs.run2 2>&1" >> s3dn.sh
 echo "rm SA?? Genome??" >> s3dn.sh
 echo "aws s3 cp s3://monorail-batch/faster_refs/$ref/star_idx/txtfiles.tar - | tar -xvf -" >> s3dn.sh
+#echo "STAR --genomeLoad LoadAndExit --genomeDir `pwd`/star_idx" >> s3dn.sh
 echo "popd" >> s3dn.sh
 echo "/usr/bin/time -v /bin/bash -x s3dn.sh > s3dn.sh.run1 2>&1" > main.jobs 
 echo "/usr/bin/time -v parallel -j2 < detar.jobs > detar.jobs.run2 2>&1" >> main.jobs
 /usr/bin/time -v parallel -j2 < main.jobs > main.jobs.run2 2>&1
 aws s3 cp s3://monorail-batch/faster_refs/$ref/gtf/exons_new.bed.gz - | zcat > gtf/exons_new.bed
 aws s3 cp s3://monorail-batch/faster_refs/$ref/gtf/split_exons_new.bed.gz - | zcat > gtf/split_exons_new.bed
-STAR --genomeLoad LoadAndExit --genomeDir `pwd`/star_idx
+#UPDATE: don't load here
+#STAR --genomeLoad LoadAndExit --genomeDir `pwd`/star_idx &
 popd
